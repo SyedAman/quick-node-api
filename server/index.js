10f6016a4510fa1c809cb8134b3bb25683a8b09b
Syed Aman <saman@elevate.com>
2018-03-08 16:31:27 -0600

    connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+
+const db = require("./config/db");
 
 const app = express();
 app.use(bodyParser.json());
 
-const database = {};
-require("./routes")(app, database);
-
 const port = 9000;
 
-app.listen(port, () => {
-  console.log(`server running on http://localhost:${port}`);
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  
+  require("./routes")(app, database);
+
+  app.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`);
+  });
+})
+
